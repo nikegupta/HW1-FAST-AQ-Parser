343a370c9d29ec4aede1183da6fe6676771cb7cd
Nikhil <Nikhil.Gupta2@ucsf.edu>
2025-01-15 12:06:08 -0800

    Second

--- example.py
+++ example.py
@@ -10,19 +10,32 @@ def main():
     """
     # Create instance of FastaParser
     # Create instance of FastqParser
+    fp = FastaParser('data/test.fa')
+    fqp = FastqParser('data/test.fq')
         
     # For each record of FastaParser, Transcribe the sequence
     # and print it to console
+    # for record in fp:
+    #     print(record)
+    #     print(transcribe(record[1]))
        
     # For each record of FastqParser, Transcribe the sequence
     # and print it to console
-
+    for record in fqp:
+        print(record)
+        # print(transcribe(record[1]))
 
     # For each record of FastaParser, Reverse Transcribe the sequence
     # and print it to console
+    # for record in fp:
+    #     print(record)
+    #     print(reverse_transcribe(record[1]))
        
     # For each record of FastqParser, Reverse Transcribe the sequence
     # and print it to console
+    # for record in fqp:
+    #     print(record)
+    #     print(reverse_transcribe(record[1]))
 
 
 """
